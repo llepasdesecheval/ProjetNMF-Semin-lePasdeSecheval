18ae207437383e0f1a84947746364fb8099b2514
llepasdesecheval <louis.lepasdesecheval@gmail.com>
2021-03-13 18:49:44 +0100

    First commit

--- ProjetNMF-Semin-lePasdeSecheval/main.cpp
+++ ProjetNMF-Semin-lePasdeSecheval/main.cpp
@@ -7,8 +7,9 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello there!\n";
     return 0;
 }
