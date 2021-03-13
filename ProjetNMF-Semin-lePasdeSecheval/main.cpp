32f40ccb230ef6fd9d6e16241eaae539348de87a
llepasdesecheval <louis.lepasdesecheval@gmail.com>
2021-03-13 18:42:36 +0100

    Initial Commit

---
+++ ProjetNMF-Semin-lePasdeSecheval/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ProjetNMF-Semin-lePasdeSecheval
+//
+//  Created by Louis le Pas de Sécheval on 13/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
